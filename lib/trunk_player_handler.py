f5949b636b8a0f0e90ad96e7b563b248628c4041
thegreatcodeholio <ian@icarey.net>
2024-07-29 01:49:22 -0400

    added error handling for trunk player upload

--- lib/trunk_player_handler.py
+++ lib/trunk_player_handler.py
@@ -22,28 +22,43 @@ def upload_to_trunk_player(player_config, call_data):
         headers = {
             "Content-Type": "application/json"
         }
-        trunk_player_json = {"auth_token": player_config.get("api_key"), "file_path": f"{generated_folder_path}/", "file_name": call_data.get("filename").split(".wav")[-1], "m4a": True}
+        trunk_player_json = {"auth_token": player_config.get("api_key"), "file_path": f"{generated_folder_path}/",
+                             "file_name": call_data.get("filename").split(".wav")[-1], "m4a": True}
         response = requests.post(url, headers=headers, json=trunk_player_json)
         response.raise_for_status()
         module_logger.info(
             f"Successfully uploaded to Trunk Player: {url}")
         return True
 
+
     except requests.exceptions.HTTPError as e:
         # HTTP error (4xx, 5xx responses)
-        error_message = response.json().get('message', response.text)
+        try:
+            error_message = response.json().get('message', response.text)
+
+        except requests.exceptions.JSONDecodeError:
+            error_message = response.text
+
         module_logger.error(f'HTTP error occurred: {response.status_code} - {error_message}')
+
     except requests.exceptions.ConnectionError as e:
         # Connection error
+
         module_logger.error(f'Connection error occurred: {e}')
+
     except requests.exceptions.Timeout as e:
         # Timeout error
+
         module_logger.error(f'Timeout error occurred: {e}')
+
     except requests.exceptions.RequestException as e:
         # Catch-all for other request exceptions
+
         module_logger.error(f'Failed uploading to Trunk Player: {e}')
+
     except Exception as e:
         # Catch-all for any other unexpected errors
+
         module_logger.error(f'An unexpected error occurred while uploading to Trunk Player {url}: {e}')
 
     return False
